9a2e0f278c809f39827b15d9e300b0eacc259233
caizhi-1005 <m13840625723@163.com>
2022-07-14 19:28:28 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ docker rm -f blockchain-event-plugin
 RPC_PORT=18535
 MysqlSourceName="root:mysql2022@tcp(47.242.7.7:13306)/cmp_chain?parseTime=true&charset=utf8&loc=Local"
 SyncRpcAddr="https://mainnet.block.caduceus.foundation"
-docker run -itd -e SyncRpcAddr=$SyncRpcAddr -e RPC_PORT=$RPC_PORT -e MysqlSourceName=$MysqlSourceName --restart=unless-stopped -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone --name blockchain-event-plugin -v $(pwd)/blockchain-event-plugin:/data  --network=host blockchain-event-plugin:1.1
+docker run -itd -e SyncRpcAddr=$SyncRpcAddr -e RPC_PORT=$RPC_PORT -e MysqlSourceName=$MysqlSourceName --restart=unless-stopped -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone --name blockchain-event-plugin -v $(pwd)/blockchain-event-plugin:/data  --network=host blockchain-event-plugin
 
 docker logs -f blockchain-event-plugin
